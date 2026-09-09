98e1edc605e2b5b4b465647ba71fcc80fe821855
KBDRT <midaskbadrat@gmail.com>
2026-09-09 15:38:33 +0300

    feat: Add logic for execute function in weather-service

--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -1,17 +1,33 @@
-import config from './config/starting-parameters.js';
-import { parseParameters } from './utils/parameters-parser.js';
+import { getGeocodingAsync, getWeatherAsync } from '../api/open-meteo-client.js';
+import { parseParameters } from '../utils/parameters-parser.js';
 
-export async function Execute() {
+export async function execute() {
   const inputParameters = parseParameters();
 
   let tasks = [];
   for (let city of inputParameters.cities) {
-    tasks.push(GetWeatherForCity(city, inputParameters.days))
+    tasks.push(getWeatherForCity(city, inputParameters.days))
   }
 
   await Promise.allSettled(tasks);
 }
 
-async function GetWeatherForCity(city, days) {
+async function getWeatherForCity(city, days) {
+  let info = {
+    city: {},
+    weather: {}
+  }
+  const cityGeocoding = await getGeocodingAsync(city);
 
+  if (cityGeocoding.founded) {
+    info.city = cityGeocoding;
+    const cityWeather = await getWeatherAsync(cityGeocoding.latitude, cityGeocoding.longitude, days);
+    info.weather = cityWeather;
+  }
+  else {
+    // console.log(`Город ${city} не найден!`);
+  }
+
+  console.log(info);
+  return info;
 }

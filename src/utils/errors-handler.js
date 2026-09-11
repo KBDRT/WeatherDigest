0728518906cddcc89992ac654b441763b18fe111
KBDRT <midaskbadrat@gmail.com>
2026-09-11 15:08:48 +0300

    fix: Fix error messagesfor API response

--- src/utils/errors-handler.js
+++ src/utils/errors-handler.js
@@ -5,12 +5,12 @@ export function handleErrors(preview, error) {
       return preview + " Превышение таймаута.";
     case "HttpError":
       if (error.status >= 400 && error.status < 500) {
-        return preview + " Ошибка клиента. Статус: ${error.status}. Текст: ${error.statusText}.";
+        return preview + ` Ошибка клиента. Статус: ${error.status}. Текст: ${error.statusText}.`;
       }
       else if (error.status >= 500 && error.status < 600) {
-        return preview + " Ошибка сервера. Статус: ${error.status}. Текст: ${error.statusText}.";
+        return preview + ` Ошибка сервера. Статус: ${error.status}. Текст: ${error.statusText}.`;
       }
-      return preview + " API вернул следующий ответ Статус: ${error.status}. Текст: ${error.statusText}.";
+      return preview + ` API вернул следующий ответ Статус: ${error.status}. Текст: ${error.statusText}.`;
     case "TypeError":
       return preview + " Проблемы с сетью.";
     case "JsonError":

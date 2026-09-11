470689c3aa15e3bcc80e016810595c79cbccde80
KBDRT <midaskbadrat@gmail.com>
2026-09-11 14:53:46 +0300

    feat: Add different errors for 4xx and 5xx response statuses

--- src/utils/errors-handler.js
+++ src/utils/errors-handler.js
@@ -2,8 +2,14 @@ export function handleErrors(preview, error) {
   const errorName = error.name;
   switch (errorName) {
     case "TimeoutError": 
-      return preview + "Превышение таймаута.";
+      return preview + " Превышение таймаута.";
     case "HttpError":
+      if (error.status >= 400 && error.status < 500) {
+        return preview + " Ошибка клиента. Статус: ${error.status}. Текст: ${error.statusText}.";
+      }
+      else if (error.status >= 500 && error.status < 600) {
+        return preview + " Ошибка сервера. Статус: ${error.status}. Текст: ${error.statusText}.";
+      }
       return preview + " API вернул следующий ответ Статус: ${error.status}. Текст: ${error.statusText}.";
     case "TypeError":
       return preview + " Проблемы с сетью.";
@@ -14,6 +20,6 @@ export function handleErrors(preview, error) {
     case "WeatherNotFound":
       return `Погода не найдена!`;
     default:
-      return preview + "Неизвестная ошибка.";
+      return preview + " Неизвестная ошибка.";
   }
 }

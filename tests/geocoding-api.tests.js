465c730baa07a56db0f9856387ef943b5b3c92e5
KBDRT <midaskbadrat@gmail.com>
2026-09-12 14:22:36 +0300

    feat: Add tests for fetch and parse functions

--- tests/geocoding-api.tests.js
+++ tests/geocoding-api.tests.js
@@ -1,28 +1,96 @@
-import { test } from 'node:test';
-import { getGeocodingAsync } from '../src/api/open-meteo-geocoding.js';
+import { describe, test } from 'node:test';
+import { getGeocodingAsync, parseResult } from '../src/api/open-meteo-geocoding.js';
 import assert from 'node:assert';
 import { NotFoundError } from '../src/errors/NotFoundError.js';
 import { JsonError } from '../src/errors/JsonError.js';
 
-test('Успех: Геокодинг города получен, при правильном названии.', async () => {
-  const cityName = "Набережные Челны";
+describe('Функция с отправкой запроса', () => {
+  test('Геокодинг получен при валидном городе.', async () => {
+    const cityName = "Набережные Челны";
 
-  const expected = {
-    name: cityName,
-    country: "Россия",
-    latitude: 55.73718,
-    longitude: 52.41961,
-    success: true
-  };
+    const expected = {
+      name: cityName,
+      country: "Россия",
+      latitude: 55.73718,
+      longitude: 52.41961,
+      success: true
+    };
 
-  assert.deepStrictEqual(await getGeocodingAsync(cityName), expected);
+    assert.deepStrictEqual(await getGeocodingAsync(cityName), expected);
+  });
+
+  test('Вызывается исключение, если город не найден.', async () => {
+    const cityName = "Нет такого города";
+
+    await assert.rejects(
+      () => getGeocodingAsync(cityName),
+      new NotFoundError("CityNotFound")
+    );
+  });
 });
 
-test('Ошибка: Город не найден.', async () => {
-  const cityName = "Нет такого города";
+describe('Функция с обработкой результата', () => {
+  test('Вызывается исключение JsonError, если нет всех полей в ответе.', async () => {
+    const response = {
+      json: async () => ({
+        results: [
+          { latitude: 55.75, longitude: 37.62 }
+        ]
+      })
+    };
 
-  await assert.rejects(
-    () => getGeocodingAsync(cityName),
-    NotFoundError
-  );
+    await assert.rejects(
+      () => parseResult(response),
+      new JsonError()
+    );
+  });
+
+  test('Вызывается исключение NotFoundError, если результат пустой.', async () => {
+    const response = {
+      json: async () => ({
+        results: [
+        ]
+      })
+    };
+
+    await assert.rejects(
+      () => parseResult(response),
+      new NotFoundError("CityNotFound")
+    );
+  });
+
+  test('Возвращается правильный объект со всеми полями, если валидный ответ.', async () => {
+    const expected = {
+      country: "Россия",
+      latitude: 55.73718,
+      longitude: 52.41961,
+      success: true
+    };
+
+    const response = {
+      json: async () => ({
+        results: [
+          { country: 'Россия', latitude: 55.73718, longitude: 52.41961, region: "RU" }
+        ]
+      })
+    };
+
+    assert.deepStrictEqual(await parseResult(response), expected);
+  });
+
+  test('Вызывается исключение NotFoundError, если ответ пустой полностью.', async () => {
+    const response = {
+      json: async () => ({
+        
+      })
+    };
+
+    await assert.rejects(
+      () => parseResult(response),
+      new NotFoundError("CityNotFound")
+    );
+  });
+
 });
+
+
--- tests/weather-api.tests.js
+++ tests/weather-api.tests.js
@@ -1,30 +1,104 @@
-import { test } from 'node:test';
-import { getGeocodingAsync } from '../src/api/open-meteo-geocoding.js';
+import { test, describe } from 'node:test';
 import assert from 'node:assert';
-import { NotFoundError } from '../src/errors/NotFoundError.js';
+import { getWeatherAsync, parseResult } from '../src/api/open-meteo-weather.js';
+import { HttpError } from '../src/errors/HttpError.js';
 import { JsonError } from '../src/errors/JsonError.js';
-import { getWeatherAsync } from '../src/api/open-meteo-weather.js';
-import { HttpError } from '../src/errors/httpError.js';
+import { NotFoundError } from '../src/errors/NotFoundError.js';
 
-// test('Успех: Геокодинг города получен, при правильном названии.', async () => {
-//   const cityName = "Набережные Челны";
+describe('Функция с отправкой запроса', () => {
+  test('Погода получена, при валидных данных.', async () => {
+    const latitude = 55.73718;
+    const longitude = 52.41961;
+    const days = 3;
 
-//   const expected = {
-//     name: cityName,
-//     country: "Россия",
-//     latitude: 55.73718,
-//     longitude: 52.41961,
-//     success: true
-//   };
+    const result = await getWeatherAsync(latitude, longitude, days);
 
-//   assert.deepStrictEqual(await getGeocodingAsync(cityName), expected);
-// });
+    assert.strictEqual(result.success, true);
+    assert.ok(Array.isArray(result.weather));
+    assert.strictEqual(result.weather.length, days);
+  });
 
-test('Ошибка: Город не найден.', async () => {
-  const cityName = "Нет такого города";
+  test('Вызывается исключение HttpError (400) Bad request, если неправильные параметры переданы в запрос.', async () => {
+    const lantitude = 10;
+    const longtitude = 10;
+    const days = -1;
 
-  await assert.rejects(
-    () => getWeatherAsync(cityName, 10, 10, -1),
-    ReferenceError
-  );
+    await assert.rejects(
+        () => getWeatherAsync(lantitude, longtitude, days),
+        HttpError
+    );
+  });
+});
+
+describe('Функция с обработкой результата', () => {
+  test('Вызывается исключение JsonError, если пустой ответ.', async () => {
+    const response = {
+      json: async () => ({
+        
+      })
+    };
+
+    await assert.rejects(
+      () => parseResult(response),
+      new JsonError()
+    );
+  });
+
+  test('Вызывается исключение JsonError, если нет всех полей в ответе.', async () => {
+    const response = {
+      json: async () => ({
+        latitude: 55.73718, longitude: 52.41961, region: "RU" 
+      })
+    };
+
+    await assert.rejects(
+      () => parseResult(response),
+      new JsonError()
+    );
+  });
+
+  test('Вызывается исключение NotFoundError, если нет заполнено значение.', async () => {
+    const response = {
+      json: async () => ({
+        daily: {time: []} 
+      })
+    };
+
+    await assert.rejects(
+      () => parseResult(response),
+      new NotFoundError("WeatherNotFound")
+    );
+  });
+
+  test('Вызывается исключение JsonError, если нет всех полей в ответе (массиве).', async () => {
+    const response = {
+      json: async () => ({
+        daily: {time: ["2026-09-12"], temperature_2m_max: 21} 
+      })
+    };
+
+    await assert.rejects(
+      () => parseResult(response),
+      new JsonError()
+    );
+  });
+
+  test('Успешный результат, если в запросе все необходимые данные.', async () => {
+    const expected = [
+      {
+        date: "2026-09-12",
+        maxTemperature: 21.2,
+        minTemperature: 11.2,
+        sumPrecipitation: 0.1
+      }
+    ]
+
+    const response = {
+      json: async () => ({
+        daily: {time: ["2026-09-12"], temperature_2m_max: [21.2], temperature_2m_min: [11.2], precipitation_sum: [0.1], m1: [10]} 
+      })
+    };
+
+    assert.deepStrictEqual(await parseResult(response), expected);
+  });
 });

c0c4e8dcd614302b1f6a70231f10165f33d0f09e
KBDRT <midaskbadrat@gmail.com>
2026-09-13 15:26:38 +0300

    fix: Fix month number

--- src/utils/date-helper.js
+++ src/utils/date-helper.js
@@ -3,12 +3,13 @@ export function getCurrentDate() {
 
   let day = date.getDate();
   if (day < 10) {
-    day = `0-${day}`;
+    day = `0${day}`;
   }
 
   let month = date.getMonth();
-  if (day < 10) {
-    day = `0-${day}`;
+  month += 1;
+  if (month < 10) {
+    month = `0${month}`;
   }
 
   const year = date.getFullYear();
